94fd7313be7e71b96a153218951e70d776d4c183
GabrielSiwa <gabrielirasiwa.8@gmail.com>
2025-01-23 20:10:02 -0700

    Initialize Basic Structure

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById('myButton').addEventListener('click', function() {
+    // Toggle the visibility of the result text
+    const result = document.getElementById('result');
+    result.classList.toggle('hidden');  // Toggle between showing and hiding the result
+  });
+  
